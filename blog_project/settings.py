aa4fe1486a575eebd7868d786156397b59a3bbf9
TpFire <tyler101price@gmail.com>
2023-06-27 11:50:59 -0400

    Set DEBUG False for live server.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=n)+=md#05vgw@ggr)d1*n+$^fu)i=o*ny5$9xoab59@nsbb%f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
